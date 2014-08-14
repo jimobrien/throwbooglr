9b3b42ad99ab7079bc21470cec9caec4a5ca72f0
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-08-14 13:00:18 -0700

    routes handler updated to parse results

--- web/basic-server.js
+++ web/basic-server.js
@@ -3,8 +3,6 @@ var app = express();
 var mongoose = require('mongoose');
 var routes = require('./routes/routes');
 
-mongoose.createConnection('mongodb://throwback:7777@ds033679.mongolab.com:33679/throwback');
-
 var port = process.env.PORT || 8080;
 var ip = process.env.URL || "localhost";
 
--- web/routes/routes.js
+++ web/routes/routes.js
@@ -8,14 +8,18 @@ module.exports = {
   },
   handler: function(req, res) {
     //check our list of sites (DB Query)
-    var url = req.url.split('/sites/')[0];
+    var url = req.url.split('/sites/')[1];
     db.find()
       .where('site').equals(url)
       .exec(function(err, sites) {
+        var results = {};
+        sites.forEach(function(site) {
+          results[date] = filepath;
+        });
         if (err) {
           fetch(url);
         } else {
-          res.json(sites);
+          res.json(results); //parse me
         }
       });
   },
