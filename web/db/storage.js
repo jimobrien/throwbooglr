5310045c230f42e1002fbca28a3b99cb190018c4
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-08-14 13:58:19 -0700

    Added Azure Support

--- web/db/storage.js
+++
@@ -1,2 +0,0 @@
-var Firebase = require('firebase');
-var myDataRef = new Firebase('https://intense-inferno-3955.firebaseio.com/');
--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,20 +1,29 @@
 var http = require('http-request');
 var archive = require('../helpers/archive-helpers');
 var db = require('../web/db/db');
+var azure = require('azure-storage');
+var blobService = azure.createBlobService();
 
-module.exports = function(pageUrl) {
+
+module.exports = function(pageUrl, callback) {
   http.get({
     url: 'http://www.' + pageUrl,
-  }, '../archives/sites/'+pageUrl, function (err, res) {
+  }, function (err, res) {
     if (err) {
       return;
     } else {
-      var date = new Date();
-      new Site({
-        site: pageUrl,
-        date: date,
-        filepath: "helloworld.txt" //FIX ME
+      blobService.createBlockBlobFromText('files', 'fileblob', res.toString(), function(error, result, response){
+        if(!error){
+          console.log(response);
+          // var date = new Date();
+          // new Site({
+          //   site: pageUrl,
+          //   date: date,
+          //   filepath: "helloworld.txt" //FIX ME
+          // });
+        }
       });
+
     }
   });
 };
